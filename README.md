9e1a5a079f999f1935b694d008ea3a8048d9bed8
Ahmed Menshawy <amenshawy@Ahmeds-MacBook-Pro.local>
2024-12-30 22:22:13 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# arabsera-aieng.github.io
---
+++ header.js
@@ -0,0 +1,31 @@
+document.write('\
+\
+<!-- Navbar -->\
+<nav class="navbar navbar-default navbar-fixed-top">\
+  <div class="container">\
+      <div class="navbar-header">\
+        <a class="navbar-brand brand" href="https://www.arabsera.org/course/ai-engineering">AI Engineering</a>\
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+                <span class="icon-bar"></span>\
+          </button>\
+      </div>\
+\
+      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">\
+\
+          <ul class="nav navbar-nav navbar-right">\
+              <li><a href="index.html#overview">Overview</a></li>\
+              <li><a href="syllabus.html">Syllabus</a></li>\
+          </ul>\
+      </div>\
+  </div>\
+</nav>\
+\
+<!-- Header -->\
+<div id="header" style="text-align:center">\
+  <h1>AI Engineering</h1>\
+  <h3>Arabsera</h3>\
+  <div style="clear:both;"></div>\
+</div>\
+');
